e47d3fdb89ef6abaa5f1f12c2e3338b0e59cc24d
slavlazar <slavlazar@users.noreply.github.com>
2021-11-22 01:48:49 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.fbed3dfafe8a036a370c5ac9c1c47dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b04a8e7a737ee4ac9960fd836956d3",
+    "url": "https://slavlazar.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "bac350d93adfc662147f",
+    "url": "https://slavlazar.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "5c06c3d27dfdae6f0049",
+    "url": "https://slavlazar.github.io/task-management-frontend/static/js/2.9a6fb6f9.chunk.js"
+  },
+  {
+    "revision": "bac350d93adfc662147f",
+    "url": "https://slavlazar.github.io/task-management-frontend/static/js/main.9f5c5e6d.chunk.js"
+  },
+  {
+    "revision": "81393663e623880f5d2b",
+    "url": "https://slavlazar.github.io/task-management-frontend/static/js/runtime~main.688195ce.js"
+  }
+]);
